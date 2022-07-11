dad17a219f91ae39429af13f8d07762a60f9a924
Anns Rv <anns.rajarethinam@gmail.com>
2022-07-11 17:26:25 -0400

    DONE

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(strr, num) {
+    if (num < 0) return error;
+    let string = '';
+    for(let i = 0; i < num; i++){
+        string += strr;
+    }
+    return string;
 };
 
 // Do not edit below this line
